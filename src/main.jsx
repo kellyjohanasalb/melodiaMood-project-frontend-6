5b5c03fd0fd3b7fe97275ba845c6ff0addfe9043
kelly saldaña <kellysalb@hotmail.com>
2023-12-19 12:38:10 -0500

    quitando estyles de main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import UserRoutes from './routes/UserRoutes.jsx';
 import AdminRoutes from './routes/AdminRoutes.jsx';
-import 'styles/styles.scss'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
