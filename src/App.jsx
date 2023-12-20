596398d0d1028aa9c94368d124733d3f331b269a
chesterlm <loaizamazocristian@gmail.com>
2023-12-20 17:36:50 -0500

    opening screen styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
+import './styles/App.scss';
+import logoMood from './assets/logo-mood.png';
+
+const App = () => {
   return (
-    <div>
-      
+    <div className="app-container">
+      <img src={logoMood} alt="Icono" className="icon-image" />
     </div>
   );
-}
+};
 
 export default App;
