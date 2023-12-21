678d9b3c194e41a541c9a68c6a83a4e8d748432f
Manuela Moreno <mamorenoc2@gmail.com>
2023-12-21 18:30:29 -0500

    First player now Desing

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../views/User/Home.jsx';
-import NowPlaying from '../views/User/NowPlaying.jsx';
+import NowPlaying from '../views/User/NowPlaying/NowPlaying.jsx';
 import Library from '../views/User/Library/Library.jsx';
 import Search from '../views/User/Search.jsx';
-import Profile from '../views/User/Profile.jsx';
+import Profile from '../views/User/profile/Profile.jsx';
 import Settings from '../views/User/Settings/Settings.jsx';
 import SignUp from '../views/User/SignUp/SignUp.jsx';
 import App from '../App.jsx';
